07ebc028dedd3e1fc0dc5d25c739b209b9b5a2d0
fabiosimplis <fabio.simplis@gmail.com>
2024-08-31 14:43:43 -0300

    first commit

---
+++ src/test/java/br/com/fjunior/dscommerce/DscommerceRestassuredApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fjunior.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceRestassuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
